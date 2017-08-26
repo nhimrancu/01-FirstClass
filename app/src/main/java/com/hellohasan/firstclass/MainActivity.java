1be93ff7eeff78b8758118dc2c3e8e30d7b3796c
Hasan Abdullah <hasan_cse91@yahoo.com>
2017-08-26 14:00:27 +0600

    done

--- app/src/main/java/com/hellohasan/firstclass/MainActivity.java
+++ app/src/main/java/com/hellohasan/firstclass/MainActivity.java
@@ -1,33 +1,26 @@
 /**
- * Android Ninja - Class No: 1 & 2 [18 August, 2017 and 19 August, 2017]
+ * Android Ninja - Class No: 1 [18 August, 2017]
  * Topics:
  * - ConstraintLayout
  * - TextView
- * - EditText
  * - Button
  * - Click listener Java method
- * - Click listener XML attribute
  * - Toast
  * - Intent (start another activity)
- * - Pass values from one activity to another activity
- * - Reusable XML layout (using `include` tag)
- * - Using third party library (Logger Library: https://hellohasan.com/2017/05/23/android-development-pretty-logger-library/)
  */
 
 package com.hellohasan.firstclass;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
-    private TextView thirdTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,16 +28,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
-        thirdTextView = (TextView) findViewById(R.id.third_text);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                thirdTextView.setText("Button Clicked");
-
-                Intent intent = new Intent(getApplicationContext(), Main2Activity.class);
-                intent.putExtra("data", "Java");
+                Intent intent = new Intent(getApplicationContext(), SecondActivity.class);
                 startActivity(intent);
 
                 Toast.makeText(getApplicationContext(), "Toast", Toast.LENGTH_SHORT).show();
